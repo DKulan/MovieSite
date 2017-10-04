f8f607f6270ae664b443f5aa4dfbb9462dac9f57
Daniel Kulangiev <danny.kulangiev@gmail.com>
2017-10-03 22:45:04 -0400

    Added all files required for movie website project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+How to generate a Movie list website:
+
+1. Run the "entertainment_center.py" file
+2. Open the "fresh_tomatoes.html" file with any web browser you choose
+
---
+++ media.py
@@ -0,0 +1,14 @@
+import webbrowser
+        
+# Create class that stores movie information for a specific movie
+class Movie():
+    """Stores all relevant information for movies"""
+    def __init__(self, title, poster_image_url, movie_desc, trailer_youtube_url):
+        self.title = title
+        self.poster_image_url = poster_image_url 
+        self.moviedesc = movie_desc
+        self.trailer_youtube_url = trailer_youtube_url
+
+        # Method that uses the web browser to show movie trailers via YouTube
+        def show_trailer(self):
+            webbrowser.open(self.movietrailer)
